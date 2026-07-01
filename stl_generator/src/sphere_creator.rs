48c53db24442305bfd0691523f3450fd15435bee
Taras Bereznyak <ndkgovno@gmail.com>
2026-07-02 01:03:57 +0300

    square-cube

--- stl_generator/src/sphere_creator.rs
+++ stl_generator/src/sphere_creator.rs
@@ -9,9 +9,9 @@ use std::ops::DerefMut;
 const PI: f32 = std::f32::consts::PI;
 
 pub struct SphereCreator {
-  a: Vec<Point>,
-  n: Vec<Point>,
-  hs: Vec<Vec<Point>>,
+  starts: Vec<Point>,
+  rounds: Vec<(f32, f32)>,
+  rounds_deep: Vec<(f32, f32)>,
 }
 
 pub fn sqr(x: f32) -> f32 {
@@ -28,49 +28,59 @@ pub fn reflectp(p: Point, a: Point, b: Point) -> Point {
   c.scale(2.0 * dot(p, c)) - p
 }
 
+fn proj(center: Point, tar: Point, p: Point) -> Point {
+  let ct = cross(center, tar).norm();
+  let cct = cross(ct, center);
+  let d = dot(center, p);
+  let c = cross(center, p).len();
+  let result = cct.scale(c) + center.scale(d);
+
+  result
+}
+
 impl SphereCreator {
   pub fn new() -> Self {
-    let edge = -0.15;
-    let c = ((edge * 2.0 + 1.0) / 3.0).sqrt();
-    let s = (1.0 - sqr(c)).sqrt();
-    let sq3 = 0.75.sqrt();
+    let mut edges = Vec::new();
 
-    let mut a = vec![
-      Point { x: s, y: 0.0, z: c },
-      Point { x: -s * 0.5, y: s * sq3, z: c },
-      Point { x: -s * 0.5, y: -s * sq3, z: c },
-    ];
+    let a0 = -Point::X;
+    let a1 = -Point::Y;
+    let a2 = a0.rotate(a1, PI / 6.0);
+    let a3 = a0.rotate(a1, PI / 3.0);
 
-    a.push(reflect(a[0], a[1], a[2]));
-    a.push(reflect(a[1], a[2], a[0]));
-    a.push(reflect(a[2], a[0], a[1]));
+    edges.push((a0, a1));
+    edges.push((a2, a1));
+    edges.push((a3, a1));
+    edges.push((a1.rotate(a2, PI / 6.0), a2));
+    edges.push((a1.rotate(a2, PI / 3.0), a2));
+    edges.push((a1.rotate(a3, PI / 6.0), a3));
+    edges.push((a1.rotate(a3, PI / 3.0), a3));
 
-    let cn = (1.0 / 3.0).sqrt();
-    let sn = (2.0 / 3.0).sqrt();
-    let mut n: Vec<Point> = vec![
-      Point { x: sn, y: 0.0, z: cn },
-      Point { x: -sn * 0.5, y: sn * sq3, z: cn },
-      Point { x: -sn * 0.5, y: -sn * sq3, z: cn },
-    ]
-    .into_iter()
-    .map(Point::norm)
-    .collect();
-    n.push(-n[0]);
-    n.push(-n[1]);
-    n.push(-n[2]);
+    let mut rounds = Vec::new();
+    let mut rounds_deep = Vec::new();
+    let mut starts = Vec::new();
 
-    let mut hs = Vec::new();
-    hs.resize_with(5, || Vec::new());
-    let ch = -0.97;
-    let sh = (1.0 - sqr(ch)).sqrt();
+    let mut first = true;
+    for e in edges {
+      let factor = 0.35;
+      for (da, rounds) in [(1.0 / 23.1, &mut rounds), (0.2 / 23.1, &mut rounds_deep)] {
+        let p = (e.0 + (e.1 - e.0).scale(factor)).norm();
+        if first {
+          starts.push(p);
+        }
+        println!("dp={}", dot(p, e.0).acos().to_degrees());
+        let a = dot(p, a0).acos();
+        rounds.push(((a + da).cos(), (a - da).cos()));
+        let p = (e.1 + (e.0 - e.1).scale(factor)).norm();
+        if first {
+          starts.push(p);
+        }
+        let a = dot(p, a0).acos();
+        rounds.push(((a + da).cos(), (a - da).cos()));
+        first = false;
+      }
+    }
 
-    hs[0] = vec![
-      Point { x: -sh, y: 0.0, z: ch },
-      Point { x: sh * 0.5, y: sh * sq3, z: ch },
-      Point { x: sh * 0.5, y: -sh * sq3, z: ch },
-    ];
-
-    Self { a, n, hs }
+    Self { starts, rounds, rounds_deep }
   }
 
   pub fn faces(&self) -> usize {
@@ -90,7 +100,7 @@ impl SphereCreator {
   }
 
   pub fn get_quality() -> usize {
-    100
+    200
   }
 
   pub fn get_size() -> f32 {
@@ -107,80 +117,38 @@ impl SphereCreator {
       return 0;
     }
 
-    let mut dists = [(f32::INFINITY, 0); 6];
-    for (i, n) in self.n.iter().enumerate() {
-      let d = 28.5 - dot(pos, *n);
-      if d < 0.0 {
-        return 0;
-      }
-      dists[i] = (d, i);
-    }
-    dists.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
-
-    let out_r = 3.0;
-    if sqr(out_r - f32::min(dists[0].0, out_r))
-      + sqr(out_r - f32::min(dists[1].0, out_r))
-      + sqr(out_r - f32::min(dists[2].0, out_r))
-      > sqr(out_r)
-    {
+    if r > 23.1 {
       return 0;
     }
 
-    fn get_dist(d: f32, c: f32) -> f32 {
-      let x1 = d - 2.0;
-      let x2 = f32::min(c - 26.5, d);
-      let x3 = f32::min(1.0 - (c - 22.5).abs(), d + 1.0);
-      let base = f32::max(f32::max(x1, x2), x3);
-      let mut d2c = f32::INFINITY;
-
-      let c2 = 0.75.sqrt();
-
-      d2c = f32::min(d2c, -d);
-      d2c = f32::min(d2c, d + 2.5);
-      d2c = f32::min(d2c, 24.5 - c);
-      d2c = f32::min(d2c, c - 20.5);
-      d2c = f32::min(d2c, 1.8 - (-(d + 1.0) * c2 + (c - 22.5) * c2));
-      d2c = f32::min(d2c, 1.8 - (-(d + 1.0) * c2 - (c - 22.5) * c2));
-      f32::max(base, d2c)
-    }
-
-    let mut index: PartIndex = 0;
-    for i in 0..self.a.len() {
-      if i < 3 || index & 7 == (1 << (i - 3)) || index & 7 == 7 - (1 << (i - 3)) {
-        let a = self.a[i];
-        let dd = get_dist(dot(pos, a), cross(pos, a).len());
-        if dd > 0.2 {
-          index += 1 << i;
-        } else if dd > -0.2 {
-          return 0;
+    if r > 22.0 {
+      for a in [Point::X, Point::Y, Point::Z, -Point::X, -Point::Y, -Point::Z] {
+        let d = dot(pos, a) / r;
+        for r in &self.rounds {
+          if d > r.0 && d < r.1 {
+            return 0;
+          }
         }
       }
     }
 
-    if index == 0 || index == 1 || index == 2 || index == 4 {
-      for &hs in &self.hs[index as usize] {
-        let hr = if r > 22.5 + 2.0 {
-          2.8
-        } else if r > 22.5 {
-          1.5
-        } else {
-          1.2
-        };
-        if dot(pos, hs) > 0.0 && cross(pos, hs).len() < hr {
-          return 0;
+    if r > 21.0 {
+      for a in [Point::X, Point::Y, Point::Z, -Point::X, -Point::Y, -Point::Z] {
+        let d = dot(pos, a) / r;
+        for r in &self.rounds_deep {
+          if d > r.0 && d < r.1 {
+            return 0;
+          }
         }
       }
-      if r > 22.5 + 0.2 {
-        index += 64
-      } else if r > 22.5 - 0.2 {
+    }
+
+    for s in &self.starts {
+      if (pos.norm().scale(23.1) - s.scale(23.1)).len() < 1.0 {
         return 0;
       }
     }
 
-    if index == 0 {
-      index = 31;
-    }
-
-    return index;
+    return 1;
   }
 }
---
+++ stl_generator/src/square_cube_creator.rs
@@ -0,0 +1,442 @@
+use common::common_for_twisty_puzzles::*;
+use common::model::*;
+use common::points3d::*;
+use common::solid::*;
+use fxhash::*;
+use num::Float;
+
+use std::cell::RefCell;
+
+const PI: f32 = std::f32::consts::PI;
+
+#[derive(Debug, Default, Clone)]
+struct NearAxis {
+  dist: f32,
+  pos: Point,
+}
+
+pub struct SquareCubeCreator {
+  axis: Vec<Point>,
+  axis1: Vec<Point>,
+  axis2: Vec<Point>,
+  add_a: FxHashMap<PartIndex, Vec<Point>>,
+  add_b: FxHashMap<PartIndex, Vec<Point>>,
+  normals: Vec<Point>,
+  groove: Vec<f32>,
+  axis_pos: RefCell<Vec<(f32, Point)>>,
+  axis_neg: RefCell<Vec<(f32, Point)>>,
+  sz: f32,
+}
+
+pub fn sqr(x: f32) -> f32 {
+  x * x
+}
+
+impl SquareCubeCreator {
+  pub fn new() -> Self {
+    let axis: Vec<_> = [
+      Point { x: 0.0, y: 0.0, z: -1.0 },
+      Point { x: 0.0, y: 0.0, z: 1.0 },
+      Point { x: 1.0, y: 0.0, z: 0.0 },
+      Point { x: -1.0, y: 0.0, z: 0.0 },
+      Point { x: 0.0, y: 1.0, z: 0.0 },
+      Point { x: 0.0, y: -1.0, z: 0.0 },
+    ]
+    .into_iter()
+    .map(|a| a.rotate(Point::Z, PI / 12.0))
+    .collect();
+
+    let mut add_a = FxHashMap::default();
+    for ia1 in 0..axis.len() {
+      let a1 = axis[ia1];
+      for ia2 in ia1 + 1..axis.len() {
+        let a2 = axis[ia2];
+        if dot(a1, a2).abs() > 0.01 {
+          continue;
+        }
+        let v = vec![
+          a1.rotate(a2, -PI * 2.0 / 6.0),
+          a1.rotate(a2, PI * 2.0 / 6.0),
+          a2.rotate(a1, -PI * 2.0 / 6.0),
+          a2.rotate(a1, PI * 2.0 / 6.0),
+        ];
+        add_a.insert((1 << ia1) + (1 << ia2), v);
+      }
+    }
+
+    let mut add_b = FxHashMap::default();
+    for ia1 in 0..axis.len() {
+      let a1 = axis[ia1];
+      if a1.z.abs() < 0.99 {
+        continue;
+      }
+      for ia2 in 0..axis.len() {
+        let a2 = axis[ia2];
+        if a2.z.abs() > 0.99 {
+          continue;
+        }
+        for ia3 in ia2 + 1..axis.len() {
+          let a3 = axis[ia3];
+          if a3.z.abs() > 0.99 {
+            continue;
+          }
+          if dot(a2, a3).abs() > 0.01 {
+            continue;
+          }
+          let vol = dot(a1, cross(a2, a3));
+          let (v, mid) = if vol > 0.0 {
+            if a1.z > 0.0 {
+              (
+                vec![a2.rotate(a1, -PI * 1.0 / 6.0), a2.rotate(a1, PI * 5.0 / 6.0)],
+                a2.rotate(a1, PI * 2.0 / 6.0),
+              )
+            } else {
+              (
+                vec![a3.rotate(a1, PI * 1.0 / 6.0), a3.rotate(a1, -PI * 5.0 / 6.0)],
+                a3.rotate(a1, -PI * 2.0 / 6.0),
+              )
+            }
+          } else {
+            if a1.z > 0.0 {
+              (
+                vec![a3.rotate(a1, -PI * 1.0 / 6.0), a3.rotate(a1, PI * 5.0 / 6.0)],
+                a3.rotate(a1, PI * 2.0 / 6.0),
+              )
+            } else {
+              (
+                vec![a2.rotate(a1, PI * 1.0 / 6.0), a2.rotate(a1, -PI * 5.0 / 6.0)],
+                a2.rotate(a1, PI * -2.0 / 6.0),
+              )
+            }
+          };
+          let av = vec![
+            mid.rotate(a1, -PI * 2.0 / 6.0),
+            mid.rotate(a1, PI * 2.0 / 6.0),
+            a1.rotate(mid, -PI * 2.0 / 6.0),
+            a1.rotate(mid, PI * 2.0 / 6.0),
+          ];
+          add_b.insert((1 << ia1) + (1 << ia2) + (1 << ia3), v);
+          add_a.insert((1 << ia1) + (1 << ia2) + (1 << ia3), av);
+        }
+      }
+    }
+
+    let normals: Vec<_> = [
+      Point { x: 0.0, y: 0.0, z: -1.0 },
+      Point { x: 0.0, y: 0.0, z: 1.0 },
+      Point { x: 1.0, y: 0.0, z: 0.0 },
+      Point { x: -1.0, y: 0.0, z: 0.0 },
+      Point { x: 0.0, y: 1.0, z: 0.0 },
+      Point { x: 0.0, y: -1.0, z: 0.0 },
+    ]
+    .into_iter()
+    .map(Point::norm)
+    .collect();
+
+    let cac = -sqr((PI * 5.0 / 12.0).cos());
+    let min_angle = (-cac / (1.0 - cac)).sqrt().acos();
+    let min_angle_core = ((PI / 4.0).cos() * (PI / 3.0).cos()).acos();
+    let max_angle = PI * 0.5;
+
+    let sz = 73.0 * 0.5;
+    let sphere_r = (1.25 + 1.5 + 5.1) / (max_angle - min_angle);
+
+    println!("angles from {min_angle} to {max_angle} r {sphere_r}");
+
+    let axis_pos = RefCell::new(Vec::new());
+    let axis_neg = RefCell::new(Vec::new());
+
+    let groove = vec![
+      (max_angle - 1.25 / (sz - 8.8)).cos(),
+      sz - 9.0,
+      (min_angle_core + 2.0 / (sz - 8.8)).cos(),
+      sz - 7.0,
+      (max_angle - 2.25 / (sz - 7.2)).cos(),
+      sz - 5.0,
+      (max_angle - 4.25 / (sz - 7.2)).cos(),
+      sz - 3.0,
+      (max_angle - 2.25 / (sz - 7.2)).cos(),
+    ];
+
+    let mut axis1 = Vec::new();
+    let mut axis2 = Vec::new();
+    for &a in &axis {
+      let a1 = a.any_perp();
+      let a2 = cross(a, a1);
+      axis1.push(a1);
+      axis2.push(a2);
+    }
+
+    Self { axis, axis1, axis2, normals, groove, axis_pos, axis_neg, add_a, add_b, sz }
+  }
+
+  pub fn faces(&self) -> usize {
+    //self.normals.len()
+    2
+  }
+
+  pub fn get_part_index(&self, pos: Point) -> PartIndex {
+    self.get_part_index_impl(pos, self.faces())
+  }
+
+  pub fn get_height(&self, current_normal: usize) -> f32 {
+    0.6
+  }
+
+  pub fn get_count(&self, current_normal: usize) -> usize {
+    1
+  }
+
+  pub fn get_name(&self, current_normal: usize) -> Option<String> {
+    None
+  }
+
+  pub fn get_sticker_index(&self, pos: crate::points2d::Point, current_normal: usize) -> PartIndex {
+    if current_normal == 0 {
+      return self.get_part_index(Point { x: pos.x, y: pos.y, z: 0.0 });
+    }
+    if current_normal == 1 {
+      return self.get_part_index(Point { x: pos.x, y: 0.0, z: pos.y });
+    }
+    0
+  }
+
+  pub fn get_quality() -> usize {
+    384
+  }
+
+  pub fn get_size() -> f32 {
+    150.0
+  }
+
+  pub fn get_part_index_impl(&self, pos: Point, current_normal: usize) -> PartIndex {
+    let r = pos.len();
+    if pos.x.abs() > 49.999 || pos.y.abs() > 49.999 || pos.z.abs() > 49.999 {
+      return 0;
+    }
+
+    let sz = self.sz;
+    let sphere_r = self.groove[1] - 4.4;
+
+    if r > self.groove[7] - 1.0 {
+      // return 0;
+    }
+
+    if r < sphere_r {
+      if r > sphere_r - 0.2 {
+        return 0;
+      }
+      if r < sphere_r - 5.0 {
+        return 0;
+      }
+      let mut frame = false;
+      for &a in &self.axis {
+        let c = dot(pos, a);
+        let s = cross(pos, a).len();
+        if c > 0.0 && s < 1.5 {
+          return 0;
+        }
+        if c.abs() < 2.5 || s < 5.0 {
+          frame = true;
+        }
+      }
+      if !frame {
+        return 0;
+      }
+      return 31;
+    }
+
+    let mut dists = [(f32::INFINITY, 0); 6];
+    for (i, n) in self.normals.iter().enumerate() {
+      let d = sz - dot(pos, *n);
+      if d < 0.0 {
+        return 0;
+      }
+      dists[i] = (d, i);
+    }
+    dists.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+
+    let out_r = 4.0;
+    if sqr(out_r - f32::min(dists[0].0, out_r))
+      + sqr(out_r - f32::min(dists[1].0, out_r))
+      + sqr(out_r - f32::min(dists[2].0, out_r))
+      > sqr(out_r)
+    {
+      return 0;
+    }
+
+    let mut index: PartIndex = 0;
+
+    let (mut shift_out, mut shift_in, inter) = get_groove(r, &self.groove, 0.03);
+    let factor = (self.groove[1] + 0.2) / r;
+    if factor > 1.0 {
+      shift_out *= factor;
+      shift_in *= factor;
+    }
+    let factor = (self.groove[3] - 0.2) / r;
+    if factor < 1.0 {
+      shift_out *= factor;
+      shift_in *= factor;
+    }
+
+    let mut axis_pos = self.axis_pos.borrow_mut();
+    let mut axis_neg = self.axis_neg.borrow_mut();
+
+    axis_pos.clear();
+    axis_neg.clear();
+
+    let mut spiral = false;
+
+    enum PositionRelAxis {
+      Inside(f32),
+      Between,
+      Outside(f32),
+    };
+
+    let get_pos_rel_axis = |pos: Point, a: Point, i: usize| {
+      let c = dot(pos, a) / r;
+      let s = cross(pos, a).len();
+      let check_in = c - shift_in;
+      if check_in > 0.0 {
+        return PositionRelAxis::Inside(check_in);
+      } else {
+        let check_out = shift_out - c;
+        if check_out > 0.0 {
+          return PositionRelAxis::Outside(check_out);
+        } else {
+          return PositionRelAxis::Between;
+        }
+      }
+    };
+
+    let mut match_axis =
+      |index: &mut PartIndex, pos: Point, a: Point, i: usize| match get_pos_rel_axis(pos, a, i) {
+        PositionRelAxis::Inside(check_in) => {
+          *index |= (1 << i);
+          axis_pos.push((check_in, a));
+          1
+        }
+        PositionRelAxis::Outside(check_out) => {
+          axis_neg.push((check_out, a));
+          -1
+        }
+        PositionRelAxis::Between => 0,
+      };
+
+    for (i, &a) in self.axis.iter().enumerate() {
+      if match_axis(&mut index, pos, self.axis[i], i) == 0 {
+        return 0;
+      }
+    }
+
+    let hole_r = if r < sphere_r { 1.5 } else { 1.2 };
+    if index.count_ones() == 1 {
+      let a = self.axis[index.ilog2() as usize];
+      let c = cross(pos, a).len();
+      let d = dot(pos, a);
+      if c > shift_out * r {
+        return 0;
+      }
+
+      if d > sz || r < self.groove[1] + 0.2 {
+        return 0;
+      }
+      if d > sz - 1.0 {
+        if d < sz - 0.8 {
+          return 0;
+        }
+        index += (dists[0].1 as PartIndex + 1) * 10000;
+      } else if d > 0.0 && c < hole_r {
+        return 0;
+      }
+      return index;
+    }
+
+    if let Some(add_b) = self.add_b.get(&index) {
+      for i in 0..add_b.len() {
+        if match_axis(&mut index, pos, add_b[i], i + 6) == 0 {
+          return 0;
+        }
+      }
+    }
+
+    if let Some(add_a) = self.add_a.get(&index) {
+      let mut dev_null = 0;
+      for i in 0..add_a.len() {
+        if match_axis(&mut dev_null, pos, add_a[i], i + 6) != 1 {
+          return 0;
+        }
+      }
+    }
+    if index.count_ones() > 3 && r < self.groove[3] + 0.2 {
+      return 0;
+    }
+
+    axis_pos.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+    axis_neg.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+
+    let mut in_sr = |a, b, d| {
+      let r = 0.096 * d;
+      if a < r && b < r {
+        return r - (sqr(r - a) + sqr(r - b)).sqrt();
+      }
+      return f32::INFINITY;
+    };
+
+    let mut minimal = axis_pos
+      .iter()
+      .min_by(|a, b| a.0.partial_cmp(&b.0).unwrap())
+      .map(|a| a.0)
+      .unwrap_or(f32::INFINITY);
+    minimal = f32::min(
+      minimal,
+      axis_neg
+        .iter()
+        .min_by(|a, b| a.0.partial_cmp(&b.0).unwrap())
+        .map(|a| a.0)
+        .unwrap_or(f32::INFINITY),
+    );
+
+    let mut rr = 1.0f32;
+
+    if index.count_ones() == 2 || index.count_ones() == 3 {
+      if r > self.groove[7] + 0.2 || r < self.groove[5] - 0.2 {
+        rr = 0.1f32;
+      } else {
+        rr = 0.2f32;
+      }
+    }
+
+    if axis_pos.len() >= 2 {
+      minimal = f32::min(minimal, in_sr(axis_pos[0].0, axis_pos[1].0, rr));
+    }
+    if axis_neg.len() >= 2 {
+      minimal = f32::min(minimal, in_sr(axis_neg[0].0, axis_neg[1].0, rr));
+    }
+    if !inter && axis_pos.len() >= 1 && axis_neg.len() >= 1 {
+      minimal = f32::min(minimal, in_sr(axis_pos[0].0, axis_neg[0].0, rr));
+    }
+
+    if minimal < 0.0 {
+      return 0;
+    }
+
+    let mut minn = f32::INFINITY;
+    for &n in &self.normals {
+      minn = f32::min(minn, cross(pos, n).len());
+    }
+
+    if dists[0].0 < 1.0 || dists[0].0 + dists[1].0 < 6.0 {
+      if dists[0].0 + 0.15 > dists[1].0 {
+        return 0;
+      }
+      if dists[0].0 < 0.8 || dists[0].0 + dists[1].0 < 5.7 {
+        index += (dists[0].1 as PartIndex + 1) * 10000;
+      } else {
+        return 0;
+      }
+    }
+
+    return index;
+  }
+}
